7cebcac93f91b37da3c7470602bb089813e34878
Mitch Myers <mmyers39@gatech.edu>
2016-02-08 21:02:13 -0500

    Initial Commit

---
+++ BuzzMovieSelector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuzzMovieSelector
+//
+//  Created by Mitchell Myers on 2/8/16.
+//  Copyright © 2016 Myers App Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
